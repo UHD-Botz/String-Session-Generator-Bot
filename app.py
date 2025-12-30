7a813676614e389c90a65b107fa5fefa5a0864a9
UHD Official <uhdnetwork@gmail.com>
2025-12-30 12:57:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'TechVJ'
+@app.route("/")
+def home():
+    return "TechVJ"
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
